3574d9970a395bca7167d1966fe206971c0edabb
Muhammed Tariq <momotrq94@gmail.com>
2022-04-18 19:41:40 -0400

    added body parser, and added post request to submit long urls, created function to generate random ids

--- express_server.js
+++ express_server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let characterString =
+    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let stringLength = characterString.length;
+  let outputArray = [];
+
+  for (let i = 0; i < 6; i++) {
+    outputArray.push(
+      characterString.charAt(Math.floor(Math.random() * stringLength))
+    );
+  }
+  outputString = outputArray.join("");
+  return outputString;
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -36,6 +52,11 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(`${generateRandomString()}`); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
