f69794c8cbef484c5b64187cf6fb1ec2594d32cb
Muhammed Tariq <momotrq94@gmail.com>
2022-04-19 21:23:47 -0400

    added login/lout feature with username cookie

--- express_server.js
+++ express_server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 function generateRandomString() {
   let characterString =
@@ -38,18 +41,20 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index.ejs", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -82,6 +87,21 @@ app.get("/u/:shortURL", (req, res) => {
   } else if (!longURL) {
     res.send("error404! page not found");
   }
+});
+
+// login and cookie send
+
+app.post("/login", (req, res) => {
+  let name = req.body.username;
+  res.cookie("username", name);
+  res.redirect(`/urls`);
+});
+
+// logout
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect(`/urls`);
 });
 
 app.listen(PORT, () => {
